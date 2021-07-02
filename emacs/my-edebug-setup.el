f1e680f55939c254cc8ef2259d05929fd842a392
noone <nobody@nowhere>
2021-07-02 12:23:50 -0400

    Correctly define edebug mode keys, the map param to evil-define-key should not be quoted

--- emacs/my-edebug-setup.el
+++ emacs/my-edebug-setup.el
@@ -1,7 +1,5 @@
 (require 'edebug)
 
-(evil-define-key "n" '(motion-state) )
-
-(evil-define-key '(motion) 'edebug-mode-map "n" 'edebug-next-mode)
-(evil-define-key '(motion) 'edebug-mode-map "s" 'edebug-step-mode)
-(evil-define-key '(motion) 'edebug-mode-map "c" 'edebug-continue-mode)
+(evil-define-key '(normal motion) edebug-mode-map "n" 'edebug-next-mode)
+(evil-define-key '(normal) edebug-mode-map "s" 'edebug-step-mode)
+(evil-define-key '(normal motion) edebug-mode-map "c" 'edebug-continue-mode)
